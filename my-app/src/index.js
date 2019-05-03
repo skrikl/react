7c602dd67739f3b4de7355790035b1878e22c0e2
skrikl <sergey.kriklivyy@gmail.com>
2019-05-03 10:36:15 +0300

    Square no longer keeps track of its status

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,19 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Компонент Square
 class Square extends React.Component {
+    // Метод render компонента Square
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button
+            className="square"
+            {/*При клике на компонент обращаемся к props этого компонента, в котором лежит property с именем onClick,
+                которое передал сюда компонент Board и в котором хранится объект handleClick(i)*/}
+            onClick={() => {this.props.onClick()} }
+        >
+          {this.props.value}
         </button>
       );
     }
   }
-  
+// Метод Board.render создает:
+// div.обертку
+//          --див.статус
+//          --дивы.строкаИгровойДоски
+//                  --элементы строки игровой доски
+//                          Элемент создается вызовом метода renderSquare самого компонента Board через this
   class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+    // Передаем в компонент Square два props: value и onClick.
+    // Их можно будет вызывать как Square.props.value и Square.props.onClick
+    // Или, в контексте Square, их можн вызывать как this.props.value и this.props.onClick
     renderSquare(i) {
-      return <Square />;
+      return <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+      />;
     }
   
     render() {
