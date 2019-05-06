a85d59bb997f2bf1442b9231f3ad24f34d173b3d
skrikl <sergey.kriklivyy@gmail.com>
2019-05-06 15:13:36 +0300

    Player status is now displayed

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -42,7 +42,7 @@ function Square(props) {
         });
     }
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
